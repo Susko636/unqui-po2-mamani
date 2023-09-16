c8365af4812ec1d998122c4d2346fb4aab409edd
Franco Mamani <mfranco260500@gmail.com>
2023-09-16 18:58:10 -0300

    Ejercicio 2

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -2,14 +2,22 @@ package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
 	
+	private int porcentajeDescuento;
+	
 	public ProductoPrimeraNecesidad(String a, Double n, boolean x) {
 		super(a, n);
 		setPrecioCuidado(x);
 	}
 	
+	public ProductoPrimeraNecesidad(String a, Double n, boolean x, int m) {
+		super(a, n);
+		setPrecioCuidado(x);
+		this.porcentajeDescuento = m;
+	}
+	
 	@Override
 	public double getPrecio() {
-		return this.precio * 0.9;
+		return this.precio - (this.precio * this.porcentajeDescuento) / 100;
 	}
 	
 }
